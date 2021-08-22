03ee1a1dcdb6f337efe50dda10dd0aafec98fe15
junkai wang <wangjunkai@papayamobile.com>
2021-08-22 17:41:12 +0800

    remove dist

--- dist/Util/index.d.ts
+++
@@ -1,3 +0,0 @@
-export declare function getFormatHtmlString(htmlStr: string, obj: any): Element;
-export declare function getTagStyleString(children: any, styleObj: any): string | null;
-export declare function getTextValue(children: any): string | null;
